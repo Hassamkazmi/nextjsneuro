fff9565eb1de91c71d463e3111e1fd1d0b69363b
Syed Hassam Kazmi <59208947+Hassamkazmi@users.noreply.github.com>
2024-01-15 02:13:36 +0500

    add feedback 8

--- components/BenefitsSection.js
+++ components/BenefitsSection.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const BenefitsSection = () => {
   return (
     <section id="features" className="bg-white benefits-section">
-      <div className="py-8 pb-24 px-4 mx-auto max-w-screen-xl sm:pt-4 lg:px-56">
+      <div className="py-8 pb-12 px-4 mx-auto max-w-screen-xl sm:pt-4 lg:px-56">
         <div className="max-w-screen-md mx-auto text-center mb-2 mt-2">
           <h2 className="text-4xl tracking-tight font-bold text-gray-900">
           ADHD causing study struggles? <br /> Not anymore.
@@ -12,7 +12,182 @@ const BenefitsSection = () => {
           <p className="mt-8 text-gray-700 sm:text-xl text-gray-900">
             Neuro Notion is built by ADHDers for ADHDers... We know your struggles, and we&apos;re making huge strides to solve them.</p>
         </div>
-        <div className="md:grid md:grid-cols-2 gap-x-16 gap-y-4 items-center">
+
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <Image
+              width={2432}
+              height={1442}
+              src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/quickadd_ben.webp"
+              alt="Flashcards Demo"
+            />
+          <div className="lg:pr-3 lg:pt-4">
+            <div className="lg:max-w-lg">
+              <dl className="mt-1 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                <div className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900 mt-2 text-1xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+                    <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clipRule="evenodd" />
+                    </svg>
+                    More Hyper-focus,
+                  </dt>
+                </div>
+                <div className="relative pl-9">
+                <dt className="inline font-semibold text-gray-900 mt-1 text-1xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+                    <svg className="absolute left-1 top-0 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
+                    </svg>
+                    Less Distractions
+                  </dt>
+                </div>
+               
+              </dl>
+              <p className="mt-3 text-lg leading-5 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                <div className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clipRule="evenodd" />
+                    </svg>
+                    Push to deploy.
+                  </dt>
+                </div>
+                <div className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
+                    </svg>
+                    SSL certificates.
+                  </dt>
+                </div>
+               
+              </dl>
+            </div>
+          </div>
+          <div>
+   
+          </div>
+        </div>
+
+        <hr/>
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+       
+          <div className="lg:pr-8 lg:pt-4">
+            <div className="lg:max-w-lg">
+              <p className="mt-2 text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">A better workflow</p>
+              <p className="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                <div className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clipRule="evenodd" />
+                    </svg>
+                    Push to deploy.
+                  </dt>
+                </div>
+                <div className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
+                    </svg>
+                    SSL certificates.
+                  </dt>
+                </div>
+               
+              </dl>
+            </div>
+          </div>
+          <Image
+              width={2432}
+              height={1442}
+              src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/flashcards_ben.webp"
+              alt="Flashcards Demo"
+            />
+          <div>
+   
+          </div>
+        </div>
+
+        <hr/>
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <Image
+              width={2432}
+              height={1442}
+              src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/focus_tools_ben.webp"
+              alt="Flashcards Demo"
+            />
+          <div className="lg:pr-8 lg:pt-4">
+            <div className="lg:max-w-lg">
+              <p className="mt-2 text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">A better workflow</p>
+              <p className="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                <div className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clipRule="evenodd" />
+                    </svg>
+                    Push to deploy.
+                  </dt>
+                </div>
+                <div className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
+                    </svg>
+                    SSL certificates.
+                  </dt>
+                </div>
+               
+              </dl>
+            </div>
+          </div>
+          <div>
+   
+          </div>
+        </div>
+
+        <hr/>
+
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+       
+       <div className="lg:pr-8 lg:pt-4">
+         <div className="lg:max-w-lg">
+           <p className="mt-2 text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">A better workflow</p>
+           <p className="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+           <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+             <div className="relative pl-9">
+               <dt className="inline font-semibold text-gray-900">
+                 <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                   <path fillRule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clipRule="evenodd" />
+                 </svg>
+                 Push to deploy.
+               </dt>
+             </div>
+             <div className="relative pl-9">
+               <dt className="inline font-semibold text-gray-900">
+                 <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                   <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
+                 </svg>
+                 SSL certificates.
+               </dt>
+             </div>
+            
+           </dl>
+         </div>
+       </div>
+       <Image
+           width={2432}
+           height={1442}
+           src="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/essayplans_ben.webp"
+           alt="Flashcards Demo"
+         />
+       <div>
+
+       </div>
+     </div>
+
+     <hr/>
+
+        {/* <div className="md:grid md:grid-cols-2 gap-x-16 gap-y-4 items-center">
           <div className="flex flex-col items-center text-center mb-2 mt-2">
             <Image
               width={320}
@@ -56,7 +231,7 @@ const BenefitsSection = () => {
             <h3 className="text-xl font-bold text-black">Work Smarter. Much Smarter.</h3>
             <p className="text-gray-500">Let AI generate Essay Plans, Summaries & more!</p>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
