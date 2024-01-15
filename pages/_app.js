a455173ee2bb06b54e54c713537d266c5d7bbb11
Syed Hassam Kazmi <59208947+Hassamkazmi@users.noreply.github.com>
2024-01-16 02:13:49 +0500

    add changes 4

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,7 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    
-          <Script
+      <Script
         id="track-pageview-pixel"
         src="https://connect.facebook.net/en_US/fbevents.js"
         onLoad={() => {
@@ -14,12 +13,10 @@ function MyApp({ Component, pageProps }) {
           // fbq('track', 'PageView');
         }}
       />
-        
       <Script
         id="vercel-speed-insights"
         src="/_vercel/insights/script.js"
-        />
-          
+      />
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -33,10 +30,15 @@ function MyApp({ Component, pageProps }) {
         <link rel="preload" href="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/logo.webp" as="image" />
         <link rel="preload" href="https://raw.githubusercontent.com/joshiebudd/notionwidgets/main/maindemo.webp" as="image" />
 
-        
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+              posthog.init('phc_3lTf840WFEVTYO7GoU20Happ4w4r4YZLpeZuzwVWd7o',{api_host:'https://eu.posthog.com'});
+            `,
+          }}
+        />
       </Head>
-
-
 
       <Component {...pageProps} />
     </>
